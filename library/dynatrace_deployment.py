3ff69d70d83a08c93160c964ab57084fe2040641
Juergen Etzlstorfer <7677092+jetzlstorfer@users.noreply.github.com>
2018-11-12 13:43:29 +0100

    reformatting

--- library/dynatrace_deployment.py
+++ library/dynatrace_deployment.py
@@ -16,11 +16,11 @@ ANSIBLE_METADATA = {'metadata_version': '1.0',
 DOCUMENTATION = '''
 ---
 module: dynatrace_deployment
-version_added: "X.X"
+version_added: "1.0"
 author: "Juergen Etzlstorfer (@jetzlstorfer)"
 short_description: Notify Dynatrace about new deployments
 description:
-   - Push deployment information to Dynatrace (see https://www.dynatrace.com/support/help/dynatrace-api/events/how-do-i-push-events-from-3rd-party-systems/)
+   - Push deployment information to Dynatrace
 options:
   tenant_url:
     description:
@@ -50,14 +50,13 @@ options:
     description:
       - A remediation action in case Dynatrace detects issues related to this deployment
     required: false
-
 requirements: []
 '''
 
 EXAMPLES = '''
 - dynatrace_deployment:
-    tenant_url: https://mytenant.live.dynatrace.com
-    api_token: XXXXXXXX
+    tenant_url: 'https://mytenant.live.dynatrace.com'
+    api_token: 'XXXXXXXX'
     attach_rules:
       tagRule:
         meTypes: 'SERVICE'
@@ -79,90 +78,91 @@ import json
 
 def main():
 
-  module = AnsibleModule(
-      argument_spec=dict(
-          tenant_url=dict(required=True),
-          api_token=dict(required=True),
-          attach_rules=dict(required=True),
-          deploymentVersion=dict(required=True),
-          deploymentName=dict(required=True),
-          deploymentProject=dict(required=False),
-          source=dict(required=False),
-          remediationAction=dict(required=False),
-          customProperties=dict(required=False)
-      ),
-      # required_one_of=[['app_name', 'application_id']],
-      supports_check_mode=True
-  )
+    module = AnsibleModule(
+        argument_spec=dict(
+            tenant_url=dict(required=True),
+            api_token=dict(required=True),
+            attach_rules=dict(required=True),
+            deploymentVersion=dict(required=True),
+            deploymentName=dict(required=True),
+            deploymentProject=dict(required=False),
+            source=dict(required=False),
+            remediationAction=dict(required=False),
+            customProperties=dict(required=False)
+        ),
+        # required_one_of=[['app_name', 'application_id']],
+        supports_check_mode=True
+    )
 
-  # build list of params
-  params = {}
+    # build list of params
+    params = {}
 
-  for item in ["deploymentVersion", "remediationAction", "deploymentName", "deploymentProject", "source", "customProperties"]:
-    if module.params[item]:
-      params[item] = module.params[item]
+    for item in ["deploymentVersion", "remediationAction", "deploymentName", "deploymentProject", "source", "customProperties"]:
+      if module.params[item]:
+        params[item] = module.params[item]
 
-  params["eventType"] = "CUSTOM_DEPLOYMENT"
-  
-  ### parse attach rules
-  attachRules={}
-  attachRulesVars = ast.literal_eval(module.params['attach_rules'])
-
-  entityIdsArr={}
-  if "entity_ids" in attachRulesVars:
-    entityIdsArr=attachRulesVars["entity_ids"].split(',')
-    attachRules["entityIds"] = entityIdsArr
-
-  tagRule={}
-  if "tagRule" in attachRulesVars:
-    tagRule=attachRulesVars["tagRule"]
-    attachRules["tagRule"] = tagRule
-
-  params["attachRules"] = attachRules
-  if "source" not in params:
-    params["source"] = "Ansible"
-  #if "source" in module.params and module.params["source"] is not None:
-  #  params["source"] = module.params["source"]
-  
-
-  
-  customProperties={}
-  if module.params["customProperties"]:
-    customPropsArr = ast.literal_eval(module.params['customProperties'])
-    params["customProperties"] = customPropsArr
-
-
-  # If we're in check mode, just exit pretending like we succeeded
-  if module.check_mode:
-    module.exit_json(changed=True)
-
-  # Send the deployment info to Dynatrace
-  dt_url = module.params["tenant_url"] + "/api/v1/events/" #?Api-Token=" + module.params["api_token"]
-  #data = urlencode(params)
-  headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Api-Token ' + module.params['api_token']
-  }
-  
-  ###### FAIL FOR DEBUG PURPOSES - TO INSPECT PAYLOAD #####
-  #module.fail_json(msg=json.dumps(params))
-  #########################################################
-  
-  ####
-  # SEND DEPLOYMENT EVENT TO DYNATRACE
-  ####
-  try:
-    response, info = fetch_url(module, dt_url, data=json.dumps(params), headers=headers)
+    params["eventType"] = "CUSTOM_DEPLOYMENT"
     
-    if info['status'] in (200, 201):
-      #module.exit_json(changed=True,meta=info)
+    ### parse attach rules
+    attachRules={}
+    attachRulesVars = ast.literal_eval(module.params['attach_rules'])
+
+    entityIdsArr={}
+    if "entity_ids" in attachRulesVars:
+      entityIdsArr=attachRulesVars["entity_ids"].split(',')
+      attachRules["entityIds"] = entityIdsArr
+
+    tagRule={}
+    if "tagRule" in attachRulesVars:
+      tagRule=attachRulesVars["tagRule"]
+      attachRules["tagRule"] = tagRule
+
+    params["attachRules"] = attachRules
+    if "source" not in params:
+      params["source"] = "Ansible"
+    #if "source" in module.params and module.params["source"] is not None:
+    #  params["source"] = module.params["source"]
+    
+
+    
+    customProperties={}
+    if module.params["customProperties"]:
+      customPropsArr = ast.literal_eval(module.params['customProperties'])
+      params["customProperties"] = customPropsArr
+
+
+    # If we're in check mode, just exit pretending like we succeeded
+    if module.check_mode:
       module.exit_json(changed=True)
-    elif info['status'] == 401:
-      module.fail_json(msg="Token Authentification failed.")
-    else:
-      module.fail_json(msg="Unable to send deployment event to Dynatrace: %s" % info)
-  except Exception as e:
-    module.fail_json(msg="Failure: " + e.message)
+
+    # Send the deployment info to Dynatrace
+    dt_url = module.params["tenant_url"] + "/api/v1/events/" #?Api-Token=" + module.params["api_token"]
+    #data = urlencode(params)
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Api-Token ' + module.params['api_token']
+    }
+    
+    ###### FAIL FOR DEBUG PURPOSES - TO INSPECT PAYLOAD #####
+    #module.fail_json(msg=json.dumps(params))
+    #########################################################
+    
+    ####
+    # SEND DEPLOYMENT EVENT TO DYNATRACE
+    ####
+    try:
+      response, info = fetch_url(module, dt_url, data=json.dumps(params), headers=headers)
+      
+      if info['status'] in (200, 201):
+        #module.exit_json(changed=True,meta=info)
+        module.exit_json(changed=True)
+      elif info['status'] == 401:
+        module.fail_json(msg="Token Authentification failed.")
+      else:
+        module.fail_json(msg="Unable to send deployment event to Dynatrace: %s" % info)
+    except Exception as e:
+      module.fail_json(msg="Failure: " + e.message)
+
 
 if __name__ == '__main__':
     main()
